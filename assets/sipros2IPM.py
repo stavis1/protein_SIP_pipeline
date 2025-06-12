bdf4669429a9f2d48ca22ff0c2e73b424880eaf9
Steven Tavis <stavis@vols.utk.edu>
2025-06-12 11:56:30 -0400

    bugfixes: corrected ipm calls and updated file locations for PSM conversion

--- assets/sipros2IPM.py
+++ assets/sipros2IPM.py
@@ -5,10 +5,10 @@ import os
 import sys
 import pandas as pd
 
-psm_file = next('sip/'+f for f in os.listdir('sip') if f.endswith('.psm.txt'))
+psm_file = next(f for f in os.listdir() if f.endswith('.psm.txt'))
 data = pd.read_csv(psm_file, sep = '\t', comment = '#')
 data['file'] = [f.replace('.FT2', '.mzML') for f in data['Filename']]
 data['seq'] = [s[1:-1] for s in data['IdentifiedPeptide']]
 data['proteins'] = [p[1:-1] for p in data['ProteinNames']]
 data = data[data['TargetMatch'] == 'T']
-data.to_csv(sys.argv[0] + '.psms', sep = '\t', index = False)
+data.to_csv(sys.argv[1] + '.psms', sep = '\t', index = False)
